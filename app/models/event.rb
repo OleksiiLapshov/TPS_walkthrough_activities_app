dd915bfebfa18996ee7f421cae0048620cc21017
Oleksii Lapshov <izraphail@gmail.com>
2025-08-17 14:57:11 +0300

    completed: custom queries

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   def free?
     price.to_f.zero? || price.blank? || price.nil?
   end
+
+  def self.upcoming
+    where("starts_at > ?", Time.now).order("starts_at")
+  end
 end
