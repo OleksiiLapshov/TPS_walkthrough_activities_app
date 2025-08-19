5af9e850794c7b3b751faa2944d9927171939d2d
Oleksii Lapshov <izraphail@gmail.com>
2025-08-20 00:31:54 +0300

    completed: model validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,14 @@
 class Event < ApplicationRecord
+
+  validates :name, :location, presence: true
+  validates :description, length: { minimum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :capacity, numericality: { only_integer: true, greater_than: 0 }
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|png)\z/i,
+    message: "Wrong file extension! Must be PNG or JPG!"
+  }
+
   def free?
     price.to_f.zero? || price.blank? || price.nil?
   end
