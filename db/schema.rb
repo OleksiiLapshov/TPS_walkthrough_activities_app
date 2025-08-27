5f5e3d25bf40df0b83450db30efda5da0c5e0bf9
Oleksii Lapshov <izraphail@gmail.com>
2025-08-28 00:15:30 +0300

    added nickname for the user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_26_202236) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_27_210854) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -39,6 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_26_202236) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   add_foreign_key "registrations", "events"
